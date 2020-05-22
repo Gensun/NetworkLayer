c018633bdf131c1dac81abe0fd557e86f3e7a56f
suncheng <suncheng@lookingedu.com>
2020-05-22 17:26:20 +0800

    optimize

--- NetworkLayer/Classes/NetworkLayer.swift
+++ NetworkLayer/Classes/NetworkLayer.swift
@@ -1,15 +1,16 @@
 //
 //  NetworkLayer.swift
-//  AAInfographics
+//  NetworkLayer
 //
 //  Created by Genie Sun on 2020/5/22.
+//  Copyright © 2020 Lookingedu. All rights reserved.
 //
 
 import Foundation
 
 public struct NetworkLayer {
     public static var forceLogoutAction: (() -> Void)?
-    public var didFinishWithSuccess: () -> Void
-    public var didFinishWithError: () -> Void
-    public var didExecuteWithError: (_ error: Error?) -> Void
+    public static var didFinishWithSuccess: (() -> Void)?
+    public static var didFinishWithError: ((_ error: Error?) -> Void)?
+    public static var didExecuteWithError: ((_ error: Error?) -> Void)?
 }
